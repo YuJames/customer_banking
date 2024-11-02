d7bddc3c0386f65a0dcb96c31202857e6d65e9ab
james yu <jyu@tradingheritage.com>
2024-11-02 18:42:13 -0500

    setup tests

---
+++ test.py
@@ -0,0 +1,11 @@
+def test_savings():
+    from savings_account import create_savings_account
+
+    results = create_savings_account(100, 15, 4)
+    print(results)
+
+def test_cd():
+    pass
+
+# test_savings()
+# test_cd()
